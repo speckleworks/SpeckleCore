1b54ac7fa76e424d5e67cbddc50ba5f0abe9b86e
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-04-18 16:56:03 +0100

    adds generic chunk list method

--- SpeckleCore/Conversion/ConverterUtilities.cs
+++ SpeckleCore/Conversion/ConverterUtilities.cs
@@ -98,4 +98,19 @@ namespace SpeckleCore
       return query;
     }
   }
+
+  /// <summary>
+  /// ref: https://stackoverflow.com/a/24087164
+  /// </summary>
+  public static class ListExtensions
+  {
+    public static List<List<T>> ChunkBy<T>( this List<T> source, int chunkSize )
+    {
+      return source
+          .Select( ( x, i ) => new { Index = i, Value = x } )
+          .GroupBy( x => x.Index / chunkSize )
+          .Select( x => x.Select( v => v.Value ).ToList() )
+          .ToList();
+    }
+  }
 }
