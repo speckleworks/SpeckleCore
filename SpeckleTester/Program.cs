05a4263617a000d6fb1c29a1b2f8c32302e69a92
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-09-04 15:22:59 +0100

    fixes refs

---
+++ SpeckleTester/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpeckleCore;
+
+namespace SpeckleTester
+{
+  class Program
+  {
+    static void Main( string[ ] args )
+    {
+      Console.WriteLine( "Hello Speckle Tester." );
+
+      SpeckleTelemetry.Initialize();
+
+      Console.WriteLine( "Your device id is: " );
+      Console.WriteLine( SpeckleTelemetry.DeviceId );
+
+      SpeckleTelemetry.RecordTestEvent( "console" );
+      SpeckleTelemetry.RecordTestEvent( "console" );
+      SpeckleTelemetry.RecordTestEvent( "console" );
+      SpeckleTelemetry.RecordTestEvent( "console" );
+      SpeckleTelemetry.RecordTestEvent( "brebit" );
+      SpeckleTelemetry.RecordTestEvent( "brebit" );
+      SpeckleTelemetry.RecordTestEvent( "brebit" );
+      SpeckleTelemetry.RecordTestEvent( "brebit" );
+      SpeckleTelemetry.RecordTestEvent( "fopper" );
+      SpeckleTelemetry.RecordTestEvent( "fopper" );
+      SpeckleTelemetry.RecordTestEvent( "fopper" );
+      SpeckleTelemetry.RecordTestEvent( "grino" );
+      SpeckleTelemetry.RecordTestEvent( "grino" );
+      SpeckleTelemetry.RecordTestEvent( "grino" );
+      SpeckleTelemetry.RecordTestEvent( "grino" );
+      SpeckleTelemetry.RecordTestEvent( "grino" );
+      SpeckleTelemetry.RecordTestEvent( "grino" );
+      SpeckleTelemetry.RecordTestEvent( "prrrft" );
+
+      Console.ReadLine();
+    }
+  }
+}
