5046864db511642c71c1687e245e3e842d487591
Matteo Cominetti <matteo@cominetti.org>
2019-10-22 16:36:37 +0100

    added SpeckleException

---
+++ Data/SpeckleException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeckleCore.Data
+{
+  public class SpeckleException : Exception
+  {
+    public SpeckleException()
+    {
+    }
+
+    public SpeckleException(string message)
+        : base(message)
+    {
+    }
+
+    public SpeckleException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+  }
+
+  public class RevitFamilyNotFoundException : SpeckleException
+  {
+    public RevitFamilyNotFoundException()
+    {
+    }
+
+    public RevitFamilyNotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public RevitFamilyNotFoundException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+  }
+}
+
+
+
