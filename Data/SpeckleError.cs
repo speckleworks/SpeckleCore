496cb1eb5d1e2f9ee17fe8c1f7c4dd6aeed61c0a
Matteo Cominetti <matteo@cominetti.org>
2019-10-22 14:30:16 +0100

    Added SpeckleError

---
+++ Data/SpeckleError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeckleCore.Data
+{
+  public class SpeckleError
+  {
+    public string Message { get; set; }
+    public string Details { get; set; }
+  }
+  public class SpeckleConversionError : SpeckleError
+  {
+    public object SourceObject { get; set; }
+  }
+}
