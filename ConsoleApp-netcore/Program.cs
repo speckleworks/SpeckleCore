756e800b2ed17097b485e4575a580a70eb411305
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-02-06 16:48:18 +0000

    random experiment

---
+++ ConsoleApp-netcore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SpeckleCore;
+
+namespace ConsoleApp_netcore
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      SpeckleCore.SpeckleInitializer.Initialize();
+      var types = SpeckleCore.SpeckleInitializer.GetTypes();
+      var test = types;
+    }
+  }
+}
